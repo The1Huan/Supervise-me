70342ef80cd01b576234f29dfbecb49309b405a2
The1Huan <130944273+The1Huan@users.noreply.github.com>
2024-05-16 06:57:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,23 @@ def Supervise_me():
                             st.write(f"Professeur : {data.iloc[index]['Teacher']}")
                             st.write(f"Email : {data.iloc[index]['email']}")
                             st.write(f"Similarité : {cos_similarity[0][index]:.2f}")
+
+                            # Display detailed statistics for the professor
                             teacher_name = data.iloc[index]['Teacher']
+                            subject_data = data.groupby(['Teacher', 'Subjects']).size().reset_index(name='Count')
+                            expertise_data = data.groupby(['Teacher', 'Area of expertise']).size().reset_index(name='Count')
+                            st.subheader(f"Details for {teacher_name}")
+                            filtered_subject_data = subject_data[subject_data['Teacher'] == teacher_name]
+                            st.write(filtered_subject_data)
+                            filtered_expertise_data = expertise_data[expertise_data['Teacher'] == teacher_name]
+                            st.write(filtered_expertise_data)
+                            st.header('Subjects Graph')
+                            subject_pivot = filtered_subject_data.pivot(index='Subjects', columns='Count', values='Count')
+                            st.bar_chart(subject_pivot)
+                            st.header('Area of expertise Graph')
+                            expertise_pivot = filtered_expertise_data.pivot(index='Area of expertise', columns='Count', values='Count')
+                            st.bar_chart(expertise_pivot)
+
                             if st.button("Voir thèses de ce professeur", key=f"teacher-{index}"):
                                 st.session_state['selected_teacher'] = teacher_name
                                 st.session_state['action'] = 'show_theses'
